3296183c0c71f590d458263d6bf3a07c3ff6e26a
Rny0Raneem <144168337+Rny0Raneem@users.noreply.github.com>
2023-09-27 09:15:34 +0300

    Update Node.java

---
+++ Project/src/project/Node.java
@@ -0,0 +1,36 @@
+
+package project;
+
+
+public class Node<T> {
+   
+	public T data;
+	public Node<T> next;
+
+	public Node () {
+		data = null;
+		next = null;
+	}
+
+	public Node (T val) {
+		data = val;
+		next = null;
+	}
+
+	public void setData(T data){
+            this.data=data;
+        }
+        public void setNext( Node next){
+            this.next=next;
+        }
+        
+        public T getData(){
+            return data;
+        }
+        
+        public Node getNext(){
+            return next;
+        }
+        
+
+}
