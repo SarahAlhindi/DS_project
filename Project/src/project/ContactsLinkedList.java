947477569a56c6d92b2c7e651c87ca221a289bb0
Shahad-BM <shahad246800@gmail.com>
2023-09-30 19:33:57 +0300

    Update ContactsLinkedList.java

--- Project/src/project/ContactsLinkedList.java
+++ Project/src/project/ContactsLinkedList.java
@@ -139,6 +139,24 @@ public class ContactsLinkedList {
 
 
 		return resultList;
-	}//end 
+	}//end
+   
+   public void remove () {
+            if (current == head) {
+                head = head.next; 
+            }
+            else {
+                Node<Contact> tmp = head;
+                while (tmp.next != current)
+                    tmp = tmp.next;
+                
+                tmp.next = current.next;
+            }
+            
+            if (current.next == null)
+                current = head;
+            else 
+                current = current.next; 
+        } // end 
 	
 } // end class
